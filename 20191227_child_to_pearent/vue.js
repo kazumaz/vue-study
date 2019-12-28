38d4eed3a6a464fcaa39365a85f831d5cfc21469
kazuma <kazumazuma1124@gmail.com>
2019-12-28 15:44:10 +0900

    add study

---
+++ 20191227_child_to_pearent/vue.js
@@ -0,0 +1,52 @@
+const UserForm = {
+  template: `
+    <div>
+      <div>ユーザー名変更フォーム</div>  
+      <input v-model='user_name' />
+      <button @click='update'>名前変更</button>
+    </div>
+  `,
+  props: {
+    userName: { type: String, required: true }
+  },
+  data() {
+    return {
+      user_name: this.userName
+    }
+  },
+  methods: {
+    update () {
+      this.$emit('update:user-name', this.user_name)
+    } 
+  }
+}
+
+const UserDetail = {
+  components: {
+    'user-form': UserForm
+  },
+  data() {
+    return {
+      user_name: 'ヤマダ タロウ'
+    }
+  },
+  template: `
+    <div>
+      <div>
+        <span>ユーザー名： {{ user_name }}</span>
+      </div>
+      <div>
+        <user-form :user-name='user_name' @update:user-name='user_name = $event'></user-form>
+      </div>
+    </div>
+  `
+}
+
+const vm = new Vue({
+  el: '#app',
+  components: {
+    'user-detail': UserDetail
+  }
+})
+
+
