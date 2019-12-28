b770273004820cb303ceddcbbec36f9fc5a8b44c
kazuma <kazumazuma1124@gmail.com>
2019-12-28 23:31:00 +0900

    add study

--- vue-project/sample-app/src/App.vue
+++ vue-project/sample-app/src/App.vue
@@ -1,10 +1,14 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <router-link to="/">ホーム</router-link> |
+      <router-link to="/about">このサイトについて</router-link>
+      <!-- <router-link to="{ name: 'home' }">Home</router-link> |
+      <router-link to="{ name: 'about' }">About</router-link> -->
+      <!-- 名前付きでリンク先を指定するメリットとして、各ルーターでパスを変えても、各ルーターリンクのパスを変える必要がないからこちらのほうが無難 -->
     </div>
     <router-view/>
+    <!-- 全てのページに共通するようなグローバルメニューなどはrouter-viewの外側に記載する -->
   </div>
 </template>
 
