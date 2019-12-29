72a0b4d02c104da8c4e535cb007d0f754ba7c9db
kazuma <kazumazuma1124@gmail.com>
2019-12-29 22:31:19 +0900

    add study

--- vue-project/sample-app/src/App.vue
+++ vue-project/sample-app/src/App.vue
@@ -1,36 +1,36 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">ホーム</router-link> |
-      <router-link to="/about">このサイトについて</router-link>
-      <!-- <router-link to="{ name: 'home' }">Home</router-link> |
-      <router-link to="{ name: 'about' }">About</router-link> -->
-      <!-- 名前付きでリンク先を指定するメリットとして、各ルーターでパスを変えても、各ルーターリンクのパスを変える必要がないからこちらのほうが無難 -->
-    </div>
-    <router-view/>
-    <!-- 全てのページに共通するようなグローバルメニューなどはrouter-viewの外側に記載する -->
-  </div>
+  <v-app>
+    <v-toolbar app>
+      <v-toolbar-side-icon @click.stop="openSideMenu"></v-toolbar-side-icon>
+      <v-toolbar-title class="headline text-uppercase">
+        <span>マイアドレス帳</span>
+      </v-toolbar-title>
+      <v-spacer></v-spacer>
+    </v-toolbar>
+    <SideNav/>
+
+    <v-content>
+    </v-content>
+  </v-app>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
 
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
+<script>
+import SideNav from './components/SideNav'
+export default {
+  name: 'App',
+  components: {
+    SideNav
+  },
+  data () {
+    return {
+      //
+    }
+  },
+  methods: {
+    openSideMenu () {
+      this.$store.dispatch('toggleSideMenu')
     }
   }
 }
-</style>
+</script>
--- vue-project/sample-app/src/store/index.js
+++ vue-project/sample-app/src/store/index.js
@@ -5,10 +5,26 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    drawer: false
   },
   mutations: {
+    //mutationのメソッドには自動でstateの値が渡ってくる。
+    //component向けのIFとなる部分がactionで、必要に応じて
+    // componentからtoggles sideメニューを実行することで
+    // drawerの更新を依頼する。
+    // 呼び出されたactionはコミットメソッドを実行して
+    // 値を更新する、,utationを呼び出す。
+    // 呼び出されたmutationはstateの値を更新する。
+    toggleSideMenu(state){
+      state.drawer = !state.drawer
+    }
   },
+  // compo
   actions: {
+    toggleSideMenu({ commit }) {
+      commit('toggleSideMenu')
+    }
+
   },
   modules: {
   }
