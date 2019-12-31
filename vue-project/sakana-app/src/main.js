1c759dd8fa3fd0885edb58a20c8fb0d798334b1f
kazuma <kazumazuma1124@gmail.com>
2019-12-31 10:41:29 +0900

    add study

--- vue-project/sakana-app/src/main.js
+++ vue-project/sakana-app/src/main.js
@@ -2,8 +2,13 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import ElementUI from 'element-ui' //追加
+import locale from 'element-ui/lib/locale/lang/ja' //追加
+import 'element-ui/lib/theme-chalk/index.css' //追加
+
 
 Vue.config.productionTip = false
+Vue.use(ElementUI, {locale}) //追加
 
 new Vue({
   router,
---
+++ vue-project/sakana-app/src/views/Sakana.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>
+    <el-row type="flex" justify="center">
+      <el-col :span="22">
+        <div>
+          <span>魚</span>
+        </div>
+      </el-col>
+    </el-row>
+  </div>
+</template>
+
+<script>           //処理
+import axios from 'axios'
+  export default {
+    name: 'Sakana',
+    data () {                                               //ここから
+      return {
+    requestForm: {
+      fieldId: null
+    },
+    sakana: [],
+    fields: []
+  }
+},
+created: async function () {                                //ここから
+  await this.refresh()
+},
+refresh: async function () {
+  const res = await axios.get('http://linkdata.org/api/1/rdf1s3598i/Sustainable_Sushi_rdf.json')
+  this.dictionaries = res.data.responseForm.wordList
+  this.fields = res.data.responseForm.fieldList
+  console.info(this.dictionaries)
+  console.info(this.Fields)
+}
+}                                                       //ここまで追記
+  }
+</script>
+
+<style scoped>
+
+</style>
